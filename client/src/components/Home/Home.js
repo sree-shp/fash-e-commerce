d03ce721281c1be7347b064040642758b398a303
Sreehari Prasad <sree2512k@gmail.com>
2023-02-17 11:59:06 +0530

    #

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Home.css";
-import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Category from "../Category/Category";
 import categories from "../Category/categories";
